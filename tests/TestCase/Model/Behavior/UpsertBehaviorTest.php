437f450bfb97be678f14983e8ba9f60ccd438f27
itosho <sho.ito@supership.jp>
2017-09-03 01:51:05 +0900

    add insert test

--- tests/TestCase/Model/Behavior/UpsertBehaviorTest.php
+++ tests/TestCase/Model/Behavior/UpsertBehaviorTest.php
@@ -7,6 +7,11 @@ use Cake\TestSuite\TestCase;
 
 class UpsertBehaviorTest extends TestCase
 {
+    /**
+     * Test subject
+     *
+     * @var \Cake\ORM\Table
+     */
     public $Articles;
     public $Behavior;
     public $fixtures = [
@@ -17,7 +22,10 @@ class UpsertBehaviorTest extends TestCase
     {
         parent::setUp();
         $this->Articles = TableRegistry::get('Itosho/StrawberryCake.Articles');
-        $this->Behavior = $this->Articles->behaviors()->Upsert;
+        $this->Behavior = $this->Articles->addBehavior('Upsert', [
+            'uniqueColumns' => ['title'],
+            'updateColumns' => ['body', 'published', 'modified']
+        ]);
     }
 
     public function tearDown()
@@ -27,8 +35,24 @@ class UpsertBehaviorTest extends TestCase
         unset($this->Behavior, $this->Articles);
     }
 
-    public function testUpsert()
+    public function testUpsertByInsert()
     {
-        $this->assertTrue(true);
+        $data = [
+            'title' => 'Fourth Article',
+            'body' => 'Fourth Article Body',
+            'published' => '1',
+            'created' => '2017-09-01 00:00:00',
+            'modified' => '2017-09-01 00:00:00'
+        ];
+        $entity = $this->Articles->newEntity($data);
+        $actual = $this->Articles->upsert($entity);
+
+        $this->assertTrue($this->Articles->exists($data), 'fail insert.');
+
+        $this->assertSame(4, $actual->id, 'return invalid id.');
+        $this->assertSame($entity->body, $actual->body, 'return invalid body.');
+        $this->assertSame($entity->published, $actual->published, 'return invalid published.');
+        $this->assertSame($entity->created, $actual->created, 'return invalid created.');
+        $this->assertSame($entity->modified, $actual->modified, 'return invalid modified.');
     }
 }
