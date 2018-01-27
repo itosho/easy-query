532309215e49641478d0b72749d411402749fd89
itosho <sho.ito@supership.jp>
2018-01-27 20:00:50 +0900

    update testBulkUpsertByInsert

--- tests/TestCase/Model/Behavior/UpsertBehaviorTest.php
+++ tests/TestCase/Model/Behavior/UpsertBehaviorTest.php
@@ -257,31 +257,17 @@ class UpsertBehaviorTest extends TestCase
             'updateColumns' => ['description', 'modified']
         ]);
 
-        $now = '2017-09-01 00:00:00';
-        $data = [
-            [
-                'name' => 'tag4',
-                'description' => 'tag4 description',
-                'created' => $now,
-                'modified' => $now
-            ],
-            [
-                'name' => 'tag5',
-                'description' => 'tag5 description',
-                'created' => $now,
-                'modified' => $now
-            ],
-            [
-                'name' => 'tag6',
-                'description' => 'tag6 description',
-                'created' => $now,
-                'modified' => $now
-            ]
-        ];
-        $entities = $this->Tags->newEntities($data);
+        $records = $this->getBaseInsertRecords();
+        $now = Chronos::now();
+        foreach ($records as $key => $val) {
+            $records[$key]['created'] = $now;
+            $records[$key]['modified'] = $now;
+        }
+
+        $entities = $this->Tags->newEntities($records);
         $this->Tags->bulkUpsert($entities);
 
-        foreach ($data as $conditions) {
+        foreach ($records as $conditions) {
             $actual = $this->Tags->exists($conditions);
             $this->assertTrue($actual, 'fail insert.');
         }
